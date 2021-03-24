84694fddda71e34e8bec84600c71fbb276a7042c
Jasmine Poindexter <poindexterjasmine@gmail.com>
2021-03-23 23:16:44 -0500

    Finishing Touches

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-import {Link} from 'react-router-dom'
-import Services from '../components/Services'
-import FeaturedRooms from '../components/FeaturedRooms';
-
-
-export default function Home() {
-    return (
-      <>
-    <Hero>
-    <Banner title="luxurious rooms" subtitle="deluxe rooms staring at $299">
-      <Link to='/rooms' className="btn-primary">
-          our rooms
-      </Link>
-    </Banner>
-
-    </Hero>
-    <Services />
-    <FeaturedRooms />
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+const home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
     </>
-    );
-}
+  );
+};
+
+export default home;
